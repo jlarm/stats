22eed5ad53bf5f8eabe89f685164007b0e35f287
jlarm <117611704+jlarm@users.noreply.github.com>
2025-03-13 14:52:32 -0500

    Updated team migration

--- database/migrations/2025_03_13_194746_create_teams_table.php
+++ database/migrations/2025_03_13_194746_create_teams_table.php
@@ -10,6 +10,8 @@ return new class extends Migration
     {
         Schema::create('teams', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('year');
             $table->timestamps();
         });
     }
