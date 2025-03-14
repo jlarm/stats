736385da5b8604a2d2dc580469b1c0e7923fb04a
jlarm <117611704+jlarm@users.noreply.github.com>
2025-03-14 10:50:15 -0500

    Added team list and create modal

--- app/Livewire/Team/Index.php
+++ app/Livewire/Team/Index.php
@@ -3,10 +3,14 @@
 namespace App\Livewire\Team;
 
 use App\Models\Team;
+use Flux;
 use Livewire\Component;
 
 class Index extends Component
 {
+    public string $name = '';
+    public int $year;
+
     public function mount()
     {
         if (! auth()->check()) {
@@ -14,10 +18,31 @@ class Index extends Component
         }
     }
 
+    public function addTeam()
+    {
+        $this->validate([
+            'name' => 'required|string|max:255',
+            'year' => 'required|integer|min:1900|max:' . date('Y'),
+        ]);
+
+        Team::create([
+            'name' => $this->name,
+            'year' => $this->year,
+        ]);
+
+        $this->reset('name', 'year');
+
+        $this->dispatch('team-added');
+
+        $this->modal('add-team')->close();
+
+        Flux::toast('Team added successfully', variant: 'success');
+    }
+
     public function render()
     {
         return view('livewire.team.index', [
             'teams' => Team::orderBy('year', 'desc')->get(),
-        ]);
+        ])->title('Teams');
     }
 }
--- resources/views/livewire/team/index.blade.php
+++ resources/views/livewire/team/index.blade.php
@@ -1,5 +1,33 @@
 <div>
-    @foreach ($teams as $team)
-        <div>{{ $team->name }}</div>
-    @endforeach
+    <div class="mx-auto max-w-lg space-y-3 max-sm:px-2">
+        <div>
+            <flux:modal.trigger name="add-team">
+                <flux:button variant="primary" icon="plus" class="w-full">Add Team</flux:button>
+            </flux:modal.trigger>
+
+            <flux:modal name="add-team" class="md:w-96">
+                <div class="space-y-6">
+                    <div>
+                        <flux:heading size="lg">Add Team</flux:heading>
+                        <flux:subheading>Add a new team to the list.</flux:subheading>
+                    </div>
+
+                    <flux:input label="Team Name" placeholder="Team Name" wire:model="name" />
+
+                    <flux:input label="Year" type="number" wire:model="year" />
+
+                    <div class="flex">
+                        <flux:spacer />
+
+                        <flux:button type="submit" variant="primary" wire:click="addTeam">Add Team</flux:button>
+                    </div>
+                </div>
+            </flux:modal>
+        </div>
+        @foreach ($teams as $team)
+            <flux:card>
+                <flux:heading size="lg">{{ $team->year }} {{ $team->name }}</flux:heading>
+            </flux:card>
+        @endforeach
+    </div>
 </div>
